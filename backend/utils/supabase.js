873ce58e117032ac126956be605b7acaa17b4024
DANDIERU <1193080635@u.icesi.edu.co>
2024-04-25 22:05:57 -0500

    Estructura base actualizada

--- backend/utils/supabase.js
+++ backend/utils/supabase.js
@@ -1,4 +1,4 @@
-const { createClient } = require('@supabase/supabase-js');
+const createClient  = require('@supabase/supabase-js');
 
 // Configura el cliente de Supabase con tu URL y clave de API
 const supabaseUrl = 'https://your-project-id.supabase.co';
