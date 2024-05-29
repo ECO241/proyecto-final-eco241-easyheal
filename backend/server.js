f9336f2276ada6a400efe55aefe7b663f1a74859
Anascalle <127751671+Anascalle@users.noreply.github.com>
2024-05-28 19:47:04 -0500

    Componente y carrito

--- backend/server.js
+++ backend/server.js
@@ -1,28 +1,36 @@
-// Importación de los módulos necesarios
 import express from 'express';
 import cors from 'cors';
+import { fileURLToPath } from 'url';
+import path from 'path';
+import { dirname } from 'path';
+
 const app = express();
-import bodyParser from 'body-parser';
 
-
-// Definir rutas
 import routerFarmacos from './routes/farmacosRouter.js';
 import routerFormulas from './routes/formulasRouter.js';
-// import routerMedicos from './routes/medicosRouter.js';
-// import routerPacientes from './routes/pacientesRouter.js';
 
- app.use(express.json());
- app.use(express.urlencoded({ extended: true }));
- app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+// Configurar el directorio de archivos estáticos
+// Cambia la ruta a una fuera del directorio `backend`
+const staticPath = path.join(__dirname, '..', 'src','frontend_medico');
+app.use(express.static(staticPath));
+
+// Ruta para servir el archivo HTML
+app.get('/farmacoss', (req, res) => {
+  res.sendFile(path.join(staticPath,  'screens', 'farmacos.html'));
+});
 
 // Enrutamientos
- app.use('/Farmacos', routerFarmacos);
+app.use('/Farmacos', routerFarmacos);
 app.use('/Formulas', routerFormulas);
-// app.use('/Medicos', routerMedicos);
-// app.use('/Pacientes', routerPacientes);
-
 
 const port = 3000;
 
 app.listen(port, () => {
-    console.log(`App listening at http://localhost:${port}`);
+  console.log(`App listening at http://localhost:${port}`);
+});
